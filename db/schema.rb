855cc89bbc9c8a745e8a1268a7f76a45c6b637e3
DAVID TAYLOR <david.nextacademy@gmail.com>
2016-10-12 12:22:32 +0800

    Frontend homepage + css + event controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161010144900) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,9 +64,9 @@ ActiveRecord::Schema.define(version: 20161010144900) do
     t.time     "start_time"
     t.time     "end_time"
     t.string   "description"
-    t.boolean  "isfree",       default: true
+    t.boolean  "isfree",      default: true
     t.float    "price"
-    t.integer  "open_spots",                  null: false
+    t.integer  "open_spots",                 null: false
     t.string   "age_range"
     t.integer  "user_id"
     t.datetime "created_at",                 null: false
